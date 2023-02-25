1ad23485d9947458d4161f30000d8b854058cd4a
iamchapita <alejandrom646@gmail.com>
2023-02-25 16:07:35 -0600

    Implementacion de vista de prueba con react

---
+++ resources/js/app.jsx
@@ -0,0 +1,11 @@
+import './bootstrap';
+import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('app'))
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
