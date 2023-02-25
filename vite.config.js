472f38e35fe0b27b329987e8b6d7e89ce16a2b39
iamchapita <alejandrom646@gmail.com>
2023-02-25 16:07:55 -0600

    Implementacion de react, react-dom y configuracion de vite

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
@@ -6,11 +7,27 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
+                'resources/js/app.jsx',
             ],
             refresh: true,
         }),
         react(),
+        reactRefresh(),
     ],
 });
+
+
+// export default ({ command }) => ({
+//     base: command === 'serve' ? '' : '/build/',
+//     publicDir: 'fake_dir_so_nothing_gets_copied',
+//     build: {
+//         manifest: true,
+//         outDir: 'public/build',
+//         rollupOptions: {
+//             input: 'resources/js/app.js',
+//         },
+//     },
+//     plugins: [
+//         reactRefresh(),
+//     ],
+// });
