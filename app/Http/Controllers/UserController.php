3ab39ed6c14db53c10730cd2f89ee61d98865efc
iamchapita <alejandrom646@gmail.com>
2023-02-26 15:11:49 -0600

    Implementando validaciones de campos para registro de usuarios

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,18 +10,65 @@ use App\Models\User;
 
 class UserController extends Controller
 {
+
+    protected function validateData($request)
+    {
+        // Extrayendo las llaves del arreglo de campos a validar
+        $keys = array_keys($request);
+
+        // Estableciendo los nombres personalizados de los atributos
+        $customAttributes = array(
+            $keys[0] => 'Nombres',
+            $keys[1] => 'Apellidos',
+            $keys[2] => 'Correo Electrónico',
+            $keys[3] => 'DNI',
+            $keys[4] => 'Teléfono',
+            $keys[5] => 'Fecha de Nacimiento',
+            $keys[6] => 'Contraseña'
+        );
+
+        // Estableciendo reglas de cada campo respectivamente
+        $rules = array(
+            $keys[0] => ['required', 'string', 'max:80'],
+            $keys[1] => ['required', 'string', 'max:80'],
+            $keys[2] => ['required', 'email', 'unique:users', 'max:50'],
+            $keys[3] => ['required', 'string', 'unique:users','regex:([0-9-]{15})'],
+            $keys[4] => ['required', 'digits:8', 'unique:users'],
+            $keys[5] => ['required', 'date'],
+            $keys[6] => ['required', 'string', 'regex:((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,35})']
+        );
+
+        // Mensajes personalizados para los errores
+        $messages = array(
+            'required' => 'El campo :attribute es requerido.',
+            'numeric' => 'El campo :attribute es de tipo numerico',
+            'date' => 'El campo :attribute es de tipo fecha.',
+            'min' => 'El campo :attribute está fuera de rango.',
+            'max' => 'El campo :attribute está fuera de rango.',
+            'unique'=> 'El campo :attribute especificado ya siendo utilizado.',
+            'string' => 'El campo :attribute debe ser una cadena de texto.',
+            'dni.regex' => 'El campo :attribute debe tener 12 caractéres de longitud(Sin Incluir Guiones).',
+            'digits' => 'El campo :attribute debe tener 8 digitos.',
+            'password.regex' => 'El campo :attribute debe contener: mayúsculas, minúsculas, números y símbolos. Debe ser mayor de 8 Caractéres.'
+        );
+
+        // Validando los datos
+        // $fields -> Campos del formulario.
+        // $rules -> Reglas para validar campos.
+        // $messages -> Mensajes personalizados para mostrar en caso de error.
+        $validator = Validator::make($request, $rules, $messages);
+
+        // Estableciendo los nombres de los atributos
+        $validator->setAttributeNames($customAttributes);
+
+        return $validator;
+    }
+
     public function register(Request $request)
     {
+
         // Validate request data
-        $validator = Validator::make($request->all(), [
-            'firstName' => 'required|string|max:80',
-            'lastName' => 'required|string|max:80',
-            'dni' => 'required|string|regex:([0-9-]{15})',
-            'email' => 'required|email|unique:users|max:255',
-            'phoneNumber' => 'required|string',
-            'birthDate' => 'required|date',
-            'password' => 'required|min:10'
-        ]);
+        $validator = $this->validateData($request->all());
 
         // Return errors if validation error occur.
         if ($validator->fails()) {
