c11af767982038ce5f0472c913a36a323a9126fc
Ajzunigag <104332622+Ajzunigag@users.noreply.github.com>
2023-02-25 21:44:16 -0600

    me aparecio actualizacion

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,11 +1,9 @@
-import './bootstrap';
-import App from './App';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+const App = () => {
+    return (
+        <div>
+            <h1>Hola</h1>
+        </div>
+    );
+}
 
-const root = ReactDOM.createRoot(document.getElementById('app'))
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-);
+export default App;
