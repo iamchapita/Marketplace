cefb052d04d9e3f3bdde1c8af769bc237b8752a9
iamchapita <alejandrom646@gmail.com>
2023-02-28 23:11:44 -0600

    Correccion en nombre de ruta

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/product', [ProductController::class, 'getProduct']);
+Route::get('/products', [ProductController::class, 'getProduct']);
 Route::get('/product/{id}', [ProductController::class, 'getProductId']);
 Route::put('/product/{id}', [ProductController::class, 'editProduct']);
 Route::post('/register', [UserController::class, 'register']);
