101d85b98fc40fd95f36733897ecffcae81c208c
iamchapita <alejandrom646@gmail.com>
2023-03-02 01:48:05 -0600

    Correccion de Controlador

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,59 +4,115 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+
+use function PHPSTORM_META\type;
 
 class ProductController extends Controller
 {
 
-    function validateData($request){
+    protected function storeImages($request, $mode = 'store')
+    {
+
+        // Obteniendo los productos almacenados en la base de datos
+        $id = $id = DB::selectOne('select count(*) as id from products')->id;
+
+        if ($mode == 'store') {
+            $id = $id + 1;
+        }
+
+        // Ruta a guardar crear y a almacenar
+        $path = 'products/' . $id;
+
+        // Se confirma si la carpeta para el producto existe, de no ser así
+        // se crea
+        if (!Storage::disk('public')->exists($path)) {
+            Storage::disk('public')->makeDirectory($path);
+        }
+
+        // Obteniendo el array de archivos enviados
+        $images = json_decode($request->only('photos')['photos'], true)['photos'];
+
+        if ($images) {
+
+            for ($i = 0; $i <  count(array_keys($images)); $i++) {
+                // Se obtiene la imagen en base64
+                $base64_image = $images[$i]['base64Image'];
+                // Se obtiene el nombre de la imagen (incluye extension)
+                $imageName = $images[$i]['name'];
+                // Se almacena el archivo en la ruta
+                Storage::disk('public')->put($path . '/' . $imageName, base64_decode($base64_image));
+            }
+            return $path;
+        }
+
+        return false;
+    }
+
+    protected function validateData($request)
+    {
         // Extrayendo las llaves del arreglo de campos a validar
-        $keys= array_keys($request);
+        $request = $request->all();
+        $keys = array_keys($request);
 
         // Estableciendo los nombres personalizados de los atributos
         $customAttributes = array(
-            $keys[0] =>'name',
-            $keys[1] =>'description',
-            $keys[2] =>'price',
-            $keys[3] =>'photos',
-            $keys[4] =>'status',
-            $keys[5] =>'isAvailable',
-            $keys[6] =>'isBanned',
-            $keys[7] =>'userIdFK',
-            $keys[8] =>'categoryIdFK'
+            $keys[0] => 'Nombre',
+            $keys[1] => 'Descripción',
+            $keys[2] => 'Precio',
+            $keys[3] => 'Fotos',
+            $keys[4] => 'Estado',
+            $keys[5] => 'Usario',
+            $keys[6] => 'Categoría'
         );
 
-         // Estableciendo reglas de cada campo respectivamente
-         $rules = array(
-            $keys[0] => ['required', 'string', 'max:80','regex:/^[a-zA-Z]+$/'],
-            $keys[1] => ['required', 'string', 'max:80'],
-            $keys[3] => ['required', 'numeric', 'max:80','min:0','max:15','regex:/^[1-9][0-9]*(?:\.[0-9]{2})?$/'],
-            $keys[4] => ['required', 'array', 'max:80'],
-            $keys[5] => ['required', 'array', 'max:80'],
-            $keys[6] => ['required', 'array', 'max:80'],
-            $keys[7] => ['required', 'string', 'max:80'],
-            //Verificar que tipo de variable es bigInteger
-            $keys[8] => ['required', 'bigInteger', 'max:80'],
-         );
-      
-         $messages = array(
+        // Estableciendo reglas de cada campo respectivamente
+        $rules = array(
+            $keys[0] => ['required', 'string', 'max:255', 'regex:/([\w \,\+\-\/\#\$\(\)]+)/'],
+            $keys[1] => ['max:255', 'string', 'regex:/([a-zA-Z \.\(\)0-9 \, \:\-\+\=\!\$\%\&\*\?\"\"\{\}\n\<\>\?\¿]+)/'],
+            $keys[2] => ['required', 'numeric', 'min:0', 'regex:/(0\.((0[1-9]{1})|([1-9]{1}([0-9]{1})?)))|(([1-9]+[0-9]*)(\.([0-9]{1,2}))?)/'],
+            $keys[3] => ['required'],
+            $keys[4] => ['required', 'max:20', 'string', 'in:Usado,Nuevo'],
+            $keys[5] => ['required', 'between:0,1', 'numeric'],
+            $keys[6] => ['required', 'numeric']
+        );
+
+        // Mensajes personalizados para los errores
+        $messages = array(
             'required' => 'El campo :attribute es requerido.',
             'min' => 'El campo :attribute está fuera de rango.',
             'max' => 'El campo :attribute está fuera de rango.',
+            'unique' => 'El campo :attribute especificado ya siendo utilizado.',
             'string' => 'El campo :attribute debe ser una cadena de texto.',
-            'numeric'=> 'El Campo : El atributo debe ser solo numeros.',
-            'bigInteger'=> 'El Campo : El atributo solo acepta datos especificos.'
+            'numeric' => 'El campo :attribute debe tener 8 digitos.',
+            'bewtween' => 'El campo :attribute debe estar ser 0 '
         );
 
+        if (count($keys) > 7) {
+
+            // Esyableciendo nombre personalizado a los campos
+            $$customAttributes[$keys[7]] = 'Disponible';
+            $$customAttributes[$keys[8]] = 'Baneado';
+
+            // Reglas de validacion de los campos
+            $rules[$keys[7]] = ['required', 'between:0,1'];
+            $rules[$keys[8]] = ['required', 'between:0,1'];
+        }
+
+        // Validando los datos
+        // $fields -> Campos del formulario.
+        // $rules -> Reglas para validar campos.
+        // $messages -> Mensajes personalizados para mostrar en caso de error.
         $validator = Validator::make($request, $rules, $messages);
 
-
+        // Estableciendo los nombres de los atributos
         $validator->setAttributeNames($customAttributes);
 
         return $validator;
     }
-
-
-
 
     /**
      * Display a listing of the resource.
@@ -71,56 +127,49 @@ class ProductController extends Controller
      */
     public function create(Request $request)
     {
-        $validator = $this->validateData($request->all());
+        $validator = $this->validateData($request);
 
-    if ($validator->fails()) {
-        return redirect()->back()->withErrors($validator)->withInput();
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()]);
+        } else {
+
+            // Obteniendo la informacion del formulario para guardar en la BD
+            $values = $request->all();
+
+            // Sobreescribe el campos photos enviados desde el frontend para almacenar
+            // la ruta donde se guardaron las imagenes en la BD.
+
+            $values['photos'] = $this->storeImages($request, 'store');
+
+            if ($values['photos'] == false) {
+                return response()->json(['message' => 'Error en las imagenes']);
+            } else {
+                DB::table('products')->insert($values);
+                return response()->json(['message' => 'Insercion Completa']);
+            }
+        }
     }
 
-    $producto = new Producto();
-    $producto->name = $request->name;
-    $producto->description = $request->description;
-    $producto->price = $request->price;
-    $producto->photos = $request->photos;
-    $producto->status = $request->status;
-    $producto->isAvailable = $request->isAvailable;
-    $producto->isBanned = $request->isBanned;
-    $producto->userIdFK = $request->userIdFK;
-    $producto->categoryIdFK = $request->categoryIdFK;
-    $producto->save();
-
-    // Redireccionar al usuario a la página de éxito
-    return redirect('/exito');
-
-    }
-    
     /**
      * Show list product.
-     * 
+     *
      */
-    public function getProduct(){
-      return response()->json(product::all(),200); 
-      
-    }
- 
-    /**
-     * 
-     */
-    public function getProductId($id){
-       $product=product::find($id);
-       if(is_null($product)){
-        return response()->json(['Mensaje'=>'Producto no encontrado'],404);
-       }
-      return response()->json($product::find($id),200);
+    public function getProduct()
+    {
+        return response()->json(product::all());
     }
 
     /**
-     * Edit products
+     *
      */
-    public function editProduct(Request $request, $id){
-        
+    public function getProductId($id)
+    {
+        $product = product::find($id);
+        if (is_null($product)) {
+            return response()->json(['message' => 'Producto no encontrado']);
         }
-
+        return response()->json([$product, 200]);
+    }
 
     /**
      * Store a newly created resource in storage.
@@ -141,17 +190,40 @@ class ProductController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-   // public function edit(Product $product)
-   // {
-        //
-   // }
+    // public function edit(Product $product)
+    // {
+    //
+    // }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Product $product)
+    public function update(Request $request, int $id)
     {
-        //
+
+        //Para buscar por id los porductos
+        $product = Product::findOrFail($id);
+
+        $validator = $this->validateData($request);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()]);
+        } else {
+            //Actualizar Productos
+            $product->name = $request->input('name');
+            $product->description = $request->input('description');
+            $product->price = $request->input('price');
+            $product->photos = $request->input('photos');
+            $product->isAvailable = $request->input('isAvailable');
+            $product->isBanned = $request->input('isBanned');
+            $product->userIdFK = $request->input('userIdFK');
+            $product->categoryIdFK = $request->input('categoryIdFK');
+            $product->save();
+        }
+
+
+        // Redirigir a la lista de productos con un mensaje de éxito
+        return response()->json(['message' => ''], 200);
     }
 
     /**
