ca03dc4c85d48addbf2499d7ede0f574cfb88e02
iamchapita <alejandrom646@gmail.com>
2023-03-09 23:34:26 -0600

    Implementacion de atributos de llenado en el modelo

--- app/Models/Direction.php
+++ app/Models/Direction.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Direction extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'departmentIdFK',
+        'municipalityIdFK',
+        'userIdFK'
+    ];
+
+
 }
