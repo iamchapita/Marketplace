22a0bc4598d3d6d5063c38aa2b25d536c4430e69
iamchapita <alejandrom646@gmail.com>
2023-03-15 00:26:06 -0600

    Implementacion de endpoints de wishlist

--- app/Http/Controllers/WishListController.php
+++ app/Http/Controllers/WishListController.php
@@ -7,65 +7,44 @@ use Illuminate\Http\Request;
 
 class WishListController extends Controller
 {
-    
-    /**
-     *  lista productos en la lista de deseos.
-     */
-    public function index($id)
-    {
-       /* $wishlist = Auth::user()->wishlist;
-        $products = $wishlist->products;
-    findall
-        return response()->json(['data' => $products]);*/
 
-        return response()->json(WishList::find( $id,'userIdFK'));
+    /**
+     *  Obtencion de todos las listas de deseos.
+     */
+    public function index()
+    {
+        $wishlist = WishList::all();
+        return response()->json([$wishlist], 200);
     }
-    
+
 
     /**
      * guardar un producto en la lista a la vez.
      */
     public function store(Request $request)
     {
-        $productId=$request->input('product_id');
-        $userId=auth()->user()->id;
+        $values =  $request->all();
+        WishList::insert($values);
 
-            // Busca si ya existe una lista de deseos para el usuario actual
-    $wishList = WishList::where('userIdFK', $userId)->first();
-    if (!$wishList) {
-        // Si no existe, crea una nueva lista de deseos para el usuario actual
-        $wishList = new WishList();
-        $wishList->userIdFK = $userId;
-        $wishList->addedDate = now();
-        $wishList->save();
-    }
-    // Busca si el producto ya está en la lista de deseos del usuario actual
-    $productWishList = ProductWishList::where('product_id', $productId)->where('wish_list_id', $wishList->id)->first();
-    if (!$productWishList) {
-        // Si el producto no está en la lista de deseos, lo agrega
-        $productWishList = new ProductWishList();
-        $productWishList->product_id = $productId;
-        $productWishList->wish_list_id = $wishList->id;
-        $productWishList->save();
+        return response()->json(['success' => true], 200);
     }
 
-    return response()->json(['success' => true]);
-    }
 
-    
 
     /**
      * eliminar un producto de la lista.
      */
-    public function delete(Product $product)
+    public function delete(Request $request)
     {
-        Auth::user()->wishlist->products()->detach($product);
-    
-        return response()->json([], 204);
-    }
-    
+        $id = $request->only('id');
 
-/******************************************************************/
+        WishList::destroy($id);
+
+        return response()->json(['message' => 'Success'], 200);
+    }
+
+
+    /******************************************************************/
     /**
      * Show the form for creating a new resource.
      */
@@ -74,8 +53,8 @@ class WishListController extends Controller
         //
     }
 
-   
-    
+
+
 
     /**
      * Display the specified resource.
